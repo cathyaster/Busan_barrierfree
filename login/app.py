0fdd9b376489cf6ce93d373ae6b79e7ab635072e
Jubi <cathyaster92@gmail.com>
2023-05-04 17:21:32 +0900

    [V_2] 회원가입

---
+++ login/app.py
@@ -0,0 +1,74 @@
+import os #디렉토리 절대 경로
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from Models import db
+from Models import User
+from flask import session #세션
+from flask_wtf.csrf import CSRFProtect #csrf
+from Form import RegisterForm, LoginForm
+app = Flask(__name__)
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+	return 'Hello World!'
+
+@app.route('/register', methods=['GET','POST']) #GET(정보보기), POST(정보수정) 메서드 허용
+def register():
+    if request.method == 'GET':
+        return render_template("register.html")
+    else:
+        userid = request.form.get('userid')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        password_2 = request.form.get('password')
+
+        if not(userid and email and password and password_2):
+            return "입력되지 않은 정보가 있습니다"
+        elif password != password_2:
+            return "비밀번호가 일치하지 않습니다"
+        else:
+            usertable=User() #user_table 클래스
+            usertable.userid = userid
+            usertable.email = email
+            usertable.password = password
+            
+            db.session.add(usertable)
+            db.session.commit()
+            return "회원가입 성공"
+        return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET','POST'])  
+def login():
+    form = LoginForm() #로그인폼
+    if form.validate_on_submit(): #유효성 검사
+        print('{}가 로그인 했습니다'.format(form.data.get('userid')))
+        session['userid']=form.data.get('userid') #form에서 가져온 userid를 세션에 저장
+        return redirect('/') #성공하면 main.html로
+    return render_template('login.html', form=form)
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.pop('userid', None)
+    return redirect('/')
+
+if __name__ == "__main__":
+    #데이터베이스---------
+    basedir = os.path.abspath(os.path.dirname(__file__)) #현재 파일이 있는 디렉토리 절대 경로
+    dbfile = os.path.join(basedir, 'db.sqlite') #데이터베이스 파일을 만든다
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
+    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True #사용자에게 정보 전달완료하면 teadown. 그 때마다 커밋=DB반영
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #추가 메모리를 사용하므로 꺼둔다
+    app.config['SECRET_KEY']='asdfasdfasdfqwerty'
+
+    csrf = CSRFProtect()
+    csrf.init_app(app)
+
+#    db = SQLAlchemy() #SQLAlchemy를 사용해 데이터베이스 저장
+    db.init_app(app) #app설정값 초기화
+    db.app = app #Models.py에서 db를 가져와서 db.app에 app을 명시적으로 넣는다
+    db.create_all() #DB생성
+
+    app.run(host="127.0.0.1", port=5000, debug=True)
---
+++ php/ex3-q.php
@@ -0,0 +1,16 @@
+<?php
+      $age =30;
+      $pay=2500;
+      while(1){
+      if($age>=65) {
+        $pay=0;
+      }
+      else {
+        break;
+      }
+    }
+      echo "나이:".$age."세<br>";
+      echo "요금:".$pay."원<br>";
+      
+
+  ?>
